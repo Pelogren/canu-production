d5a65e244399264de45b375c8909a3ea980adeab
Canuserver <canu@creativityserver.com>
2019-07-09 16:46:20 +0200

    redux outcommented

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60d47ecc91cdfe1371a68d56b9029d6e.js"
+  "/precache-manifest.817a639b81d7e7a61f6817611babcd32.js"
 );
 
 workbox.clientsClaim();
